246107752ca608662f94c38007d6d89a13d5bc5b
Mike <mike@BigMac1.local>
2016-08-22 10:53:07 -0600

    this is my hello world app for msse-650 wk1

---
+++ ios1/wk1/HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Mike on 8/22/16.
+//  Copyright © 2016 msse650. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
