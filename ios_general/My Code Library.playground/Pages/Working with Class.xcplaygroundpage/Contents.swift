bf30d12868ac03b3e2990283a002e2d972f73bc9
Mike <mike@BigMac1.local>
2016-08-29 21:22:04 -0600

    committing playgrounds

--- ios_general/My Code Library.playground/Pages/Working with Class.xcplaygroundpage/Contents.swift
+++ ios_general/My Code Library.playground/Pages/Working with Class.xcplaygroundpage/Contents.swift
@@ -1,7 +1,35 @@
-//: [Previous](@previous)
 
-import Foundation
 
-var str = "Hello, playground"
+import UIKit
 
-//: [Next](@next)
+class Player {
+    var fname: String
+    var lname: String
+    var team:  String
+    var dob: String
+    var age = 0
+    
+    init(fname: String,lname: String,team: String,dob: String) {
+        self.fname = fname
+        self.lname = lname
+        self.team = team
+        self.dob = dob
+    }
+    
+    //Fundtion to calulate Age
+    // Usage:
+    // Parameter: Date {String mm/dd/yyyy format}
+    // Return: Age {Int}
+    func calcAge(birthday:String) -> Int{
+        let dateFormater = DateFormatter()
+        dateFormater.dateFormat = "MM/dd/yyyy"
+        let birthdayDate = dateFormater.date(from: self.dob)
+        let calendar: NSCalendar! = NSCalendar(calendarIdentifier: NSCalendar.Identifier.gregorian)
+        let now: NSDate! = NSDate()
+        let calcAge = calendar.components(.year, from: birthdayDate!, to: now as Date, options: [])
+        //let calcAge = calendar.components(.year, from: <#T##Date#>, to: <#T##Date#>, options: <#T##NSCalendar.Options#>)
+        let age = calcAge.year
+        return age!
+    }
+        
+}
